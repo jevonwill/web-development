fcb0fdda788a289992c687ce1ceab5fb0a732b94
root <root@ip-172-31-47-99.us-east-2.compute.internal>
2020-06-02 23:29:30 +0000

    Added migrations

--- webapp/blog/models.py
+++ webapp/blog/models.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 
 class Review(models.Model):
         title = models.CharField(max_length=100)
-        content = models.Textfield()
+        content = models.TextField()
         date_posted = models.DateTimeField(auto_now_add=True) 
         author = models.ForeignKey(User, on_delete=models.CASCADE)                  
