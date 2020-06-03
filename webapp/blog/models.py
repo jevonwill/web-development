dba8760835880e9e75b4e52fc60986bda475ecae
jevonwill <jevonwilliamsjr@gmail.com>
2020-06-02 22:57:06 -0400

    Removed max_length from IntegerField

--- webapp/blog/models.py
+++ webapp/blog/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Review(models.Model):
         title = models.CharField(max_length=100)
         content = models.TextField()
-        rating = models.IntegerField(max_length=1) 
+        rating = models.IntegerField() 
         author = models.ForeignKey(User, on_delete=models.CASCADE)   
 
         def __str__(self):
