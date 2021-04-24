9f7147a7224820039bd4f178b9496bc6debeadfc
jevonwill <jevonwilliamsjr@gmail.com>
2021-04-24 19:19:42 +0000

    Added apache install to bash script

--- apache_install.sh
+++ apache_install.sh
@@ -2,3 +2,10 @@
 #Apache installation script, completely original with awk/grep regex expressions
 # Authored by Jevon Williams
 # 4/23/2021 6:14PM
+
+
+
+#Install Apache
+sudo apt update
+sudo apt install apache2 -y
+
