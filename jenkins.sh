dbdedaa834dd338c15e9fe2edd6acae04bddf1d3
jevonwill <jevonwilliamsjr@gmail.com>
2021-05-14 19:45:40 +0000

    Added jenkins script

---
+++ jenkins.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#Jenkins installation script
+#Authored by Jevon Williams 
+#2/26/2020
+
+
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk
+java -version
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins
+systemctl status jenkins
